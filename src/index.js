deb857890df024c53905330cd605d49524a071a0
yentl.de.brabander <yentl.de.brabander@student.howest.be>
2024-01-13 21:21:34 +0100

    Add MQTT support for real-time data updates

--- src/index.js
+++ src/index.js
@@ -16,5 +16,31 @@ module.exports = {
    * This gives you an opportunity to set up your data model,
    * run jobs, or perform some special logic.
    */
-  bootstrap(/*{ strapi }*/) {},
+  bootstrap(/*{ strapi }*/) {
+    console.log('MQTT bootstrap');
+    const mqtt = require('mqtt');
+    const client = mqtt.connect(process.env.MQTT_HOST);
+    const mqttHandler = require('./services/mqttHandler');
+
+    client.on('connect', () => {
+      console.log('MQTT connected');
+      client.subscribe(process.env.MQTT_TOPIC);
+    });
+
+    client.on('message', (topic, message) => {
+      console.log(`Received message on topic ${topic}: ${message.toString()}`);
+      mqttHandler.handleMqttMessage(strapi, message.toString());
+    });
+
+    client.on('error', (error) => {
+      console.error('MQTT error:', error);
+    });
+
+    // Cleanup on application shutdown
+    process.on('SIGINT', () => {
+      console.log('Shutting down MQTT listener');
+      client.end();
+      process.exit();
+    });
+  },
 };
---
+++ src/services/mqttHandler.js
@@ -0,0 +1,40 @@
+// services/mqttHandler.js
+
+const { json } = require("express");
+
+module.exports = {
+    handleMqttMessage: async (strapi, message) => {
+        try {
+            const jsonData = JSON.parse(message);
+            const deviceId = jsonData.device_id;
+    
+            // Query Strapi database to find the plant by device_id
+            const plant = await strapi.query('api::plant.plant').findOne({ device_id: deviceId });
+            if (plant.device_id != deviceId) {
+                console.log('Plant not found:', deviceId);
+                return;
+            } else {
+                if (plant) {
+                    console.log('Found plant:', plant);
+                    plant.Temperature = parseFloat(jsonData.temperature.toFixed(1));
+                    plant.Light = parseFloat(jsonData.light_intensity.toFixed(2));
+                    if (jsonData.soil_moisture == 0) {
+                        plant.Hydrated = true;
+                    }
+                    else {
+                        plant.Hydrated = false;
+                    }
+                    console.log('Updated plant:', plant);
+                    await strapi.query('api::plant.plant').update({ where: {id: plant.id}, data: {
+                        Light: plant.Light,
+                        Temperature: plant.Temperature,
+                        Hydrated: plant.Hydrated
+                    }});
+                }
+            }
+        } catch (error) {
+            console.error('Error processing MQTT message:', error);
+        }
+    },
+};
+  
